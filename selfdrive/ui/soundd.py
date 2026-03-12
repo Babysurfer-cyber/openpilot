b17ad97a8a970b8b8b84fbf6f320c982f6a84fab
Babysurfer-cyber <dlwlsgh0201@gmail.com>
2026-03-12 20:46:15 +0900

    Fix duplicate function definition in soundd.py
    Removed duplicate function definition for get_audible_alert.

--- selfdrive/ui/soundd.py
+++ selfdrive/ui/soundd.py
@@ -205,7 +205,6 @@ class Soundd:
     return new_alert
 
   def get_audible_alert(self, sm):
-  def get_audible_alert(self, sm):
     if sm.updated['selfdriveState']:
       new_alert = sm['selfdriveState'].alertSound.raw
       new_alert = self.update_carrot_alert(sm, new_alert)
