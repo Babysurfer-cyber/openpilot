cc17f8635e9558b1ee16572bb8ff4b1f0aa5b828
Babysurfer-cyber <dlwlsgh0201@gmail.com>
2026-03-18 21:06:38 +0900

    차선 두께 수정
    차선 두께 수정
    기존 0.025, 0.05
    수정 0.05, 0.08 
    907, 908 라인

--- selfdrive/ui/carrot.cc
+++ selfdrive/ui/carrot.cc
@@ -905,7 +905,7 @@ protected:
         right_lane_line = sm["carState"].getCarState().getRightLaneLine();
         for (int i = 0; i < std::size(lane_line_vertices); i++) {
             lane_line_probs[i] = model_lane_line_probs[i];
-            float line_width = 0.05;
+            float line_width = 0.05; 
             if (i == 1 && left_lane_line >= 20) line_width = 0.08;
             update_line_data(s, model_lane_lines[i], line_width, 0.0, 0.0, &lane_line_vertices[i], max_idx);
             if (i == 1) {
