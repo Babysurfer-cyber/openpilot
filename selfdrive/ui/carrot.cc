2e08d98a0679b4d1120f25059c771e0c8a2d2744
Babysurfer-cyber <dlwlsgh0201@gmail.com>
2026-03-21 18:50:57 +0900

    0 대신 - 표시
    0 대신 - 으로 표기
    
    2520라인

--- selfdrive/ui/carrot.cc
+++ selfdrive/ui/carrot.cc
@@ -2521,12 +2521,11 @@ public:
                 disp_speed = nRoadLimitSpeed;
 		            disp_speed = (int)(disp_speed * ((s->scene.is_metric)?1.0:KM_TO_MILE) + 0.5);
                 
-                // 🎯 [추가 수정] 제한속도가 0보다 클 때만 과속 검사 로직 작동!
+                // 🎯 [최종 수정] 제한속도가 0보다 클 때만 과속 검사 로직 작동!
                 limit_color = (disp_speed > 0 && v_ego * 3.6 > disp_speed + 2) ? COLOR_RED_ALPHA(210) : COLOR_WHITE_ALPHA(210);
                 
                 ui_draw_text(s, dx, dy - 45, "LIMIT", 30, COLOR_WHITE, BOLD);
             }
-
         }
 
         if (show_device_state) {
