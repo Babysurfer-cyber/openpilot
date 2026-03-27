fa0936a38df537d561906cffc025b7f93d4e1b24
Babysurfer-cyber <dlwlsgh0201@gmail.com>
2026-03-27 14:54:38 +0900

    차간거리 테두리 검은색으로 변경
    2462

--- selfdrive/ui/carrot.cc
+++ selfdrive/ui/carrot.cc
@@ -2459,7 +2459,7 @@ public:
         gap = 3;
 #endif
         for (int i = 0; i < gap; i++) {
-            ui_fill_rect(s->vg, { (int)(dx), (int)(dy - ddy*(i+1) + 2), (int)70, (int)ddy-2}, COLOR_GREEN_ALPHA(210), 4, 3, &white_color);
+            ui_fill_rect(s->vg, { (int)(dx), (int)(dy - ddy*(i+1) + 2), (int)70, (int)ddy-2}, COLOR_GREEN_ALPHA(190), 4, 3, 0);
         }
 
         char gear_str[32] = "R";
