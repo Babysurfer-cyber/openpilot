6e7d374abaf030a0e2fe63f238939b55107a302c
Babysurfer-cyber <dlwlsgh0201@gmail.com>
2026-04-02 18:29:59 +0900

    당크 온/오프 안내음 변경

--- selfdrive/car/car_specific.py
+++ selfdrive/car/car_specific.py
@@ -174,10 +174,10 @@ class CarSpecificEvents:
 
     if CC.enabled:
       if self.vCruise_prev == 0 and CS.vCruise > 0:
-        events.add(EventName.audiolongEngaged)
+        events.add(EventName.audio0)
 
     if self.carrotCruise_prev != CS.carrotCruise:
-      events.add(EventName.audiolongEngaged)
+      events.add(EventName.audio0)
 
     self.carrotCruise_prev = CS.carrotCruise
     self.vCruise_prev = CS.vCruise
