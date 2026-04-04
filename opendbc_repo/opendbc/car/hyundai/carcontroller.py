14a1680884c154193c13d2c6c8fd137e28b6b696
Babysurfer-cyber <dlwlsgh0201@gmail.com>
2026-04-04 19:05:25 +0900

    후진시 깜빡이 테스트

--- opendbc_repo/opendbc/car/hyundai/carcontroller.py
+++ opendbc_repo/opendbc/car/hyundai/carcontroller.py
@@ -337,7 +337,18 @@ class CarController(CarControllerBase):
           
       # blinkers
       if hda2 and self.CP.flags & HyundaiFlags.ENABLE_BLINKERS:
-        can_sends.extend(hyundaicanfd.create_spas_messages(self.packer, self.CAN, self.frame, CC.leftBlinker, CC.rightBlinker))
+        left_blinker = CC.leftBlinker
+        right_blinker = CC.rightBlinker
+        
+        # =========================================================
+        # ▼ [테스트] 후진(R) 기어 시 좌측 깜빡이 강제 점등 로직 ▼
+        # =========================================================
+        if CS.out.gearShifter == structs.CarState.GearShifter.reverse:
+          left_blinker = True   # 후진 시 무조건 왼쪽 깜빡이 켜기!
+          right_blinker = False # 오른쪽은 끄기
+        # =========================================================
+        
+        can_sends.extend(hyundaicanfd.create_spas_messages(self.packer, self.CAN, self.frame, left_blinker, right_blinker))
 
       if self.camera_scc_params in [2, 3]:
         self.canfd_toggle_adas(CC, CS)
