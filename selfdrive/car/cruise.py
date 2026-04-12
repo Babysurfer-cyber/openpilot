e4ebcaa66687cabaf71993568cadff0e534f3b36
Babysurfer-cyber <dlwlsgh0201@gmail.com>
2026-04-12 14:32:45 +0900

    크루즈 롱클릭 주기 0.8초로 변경

--- selfdrive/car/cruise.py
+++ selfdrive/car/cruise.py
@@ -161,7 +161,7 @@ class VCruiseCarrot:
     self.long_pressed = False
     self.button_cnt = 0
     self.button_prev = ButtonType.unknown
-    self.button_long_time = 50
+    self.button_long_time = 80
 
     self.is_metric = True
 
