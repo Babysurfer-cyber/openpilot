3eadc111c48210ebfd92e62184622d800895ff2a
Babysurfer-cyber <dlwlsgh0201@gmail.com>
2026-04-21 18:47:18 +0900

    테스트

--- opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
@@ -282,7 +282,7 @@ def create_acc_cancel(packer, CP, CAN, cruise_info_copy):
   })
   return packer.make_can_msg("SCC_CONTROL", CAN.ECAN, values)
 
-def create_lfahda_cluster(packer, CS, CAN, long_active, lat_active):
+def create_lfahda_cluster(packer, CS, CAN, long_active, lat_active, hud_control):
 
 
   if CS.lfahda_cluster is not None:
@@ -295,7 +295,7 @@ def create_lfahda_cluster(packer, CS, CAN, long_active, lat_active):
     values["LFA_OptUsmSta"] = 2
     values["HDA_OptUsmSta"] = 2
   values["HDA_CntrlModSta"] = 2 if long_active else 0
-  values["HDA_LFA_SymSta"] = 2 if lat_active else 0
+  values["HDA_LFA_SymSta"] = 2 if hud_control.leadVisible else 0
   return [packer.make_can_msg("LFAHDA_CLUSTER", CAN.ECAN, values, rx_counter=rx_counter)]
 
 def create_lfa_icon_non_camera_scc(packer, CS, CAN, CC):
