1c9ddd487a338e7ffa91b75d223461d8207d8d4c
Babysurfer-cyber <dlwlsgh0201@gmail.com>
2026-04-23 21:14:34 +0900

    원복

--- opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
@@ -282,8 +282,8 @@ def create_acc_cancel(packer, CP, CAN, cruise_info_copy):
   })
   return packer.make_can_msg("SCC_CONTROL", CAN.ECAN, values)
 
-# 함수 입구에 hud_control 꼭 추가!
-def create_lfahda_cluster(packer, CS, CAN, long_active, lat_active, hud_control):
+def create_lfahda_cluster(packer, CS, CAN, long_active, lat_active):
+
 
   if CS.lfahda_cluster is not None:
     values = copy.copy(CS.lfahda_cluster)
@@ -294,18 +294,8 @@ def create_lfahda_cluster(packer, CS, CAN, long_active, lat_active, hud_control)
     rx_counter = None
     values["LFA_OptUsmSta"] = 2
     values["HDA_OptUsmSta"] = 2
-    
   values["HDA_CntrlModSta"] = 2 if long_active else 0
-  
-  # ▼▼▼ 유저님 맞춤형 전방 차량 연동 + 조향 방어 로직 ▼▼▼
-  if lat_active:
-    # 오픈파일럿이 조향 중일 때: 
-    # 앞차가 보이면 2(초록 핸들+녹색선), 안 보이면 1(회색 핸들, 조향은 안 풀림!)
-    values["HDA_LFA_SymSta"] = 2 if hud_control.leadVisible else 1
-  else:
-    # 조향을 껐을 때는 안전하게 0(숨김)
-    values["HDA_LFA_SymSta"] = 0
-
+  values["HDA_LFA_SymSta"] = 2 if lat_active else 0
   return [packer.make_can_msg("LFAHDA_CLUSTER", CAN.ECAN, values, rx_counter=rx_counter)]
 
 def create_lfa_icon_non_camera_scc(packer, CS, CAN, CC):
