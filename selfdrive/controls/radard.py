52f10649b1577103d812bd36fafd7d62ceb1f118
Babysurfer-cyber <dlwlsgh0201@gmail.com>
2026-08-12 17:26:17 +0900

    코너레이더 끼어들기 보완

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -884,7 +884,7 @@ class RadarD:
         if cur_lat <= 1.6:
             # ① 초근접 구역 (내차 반폭 + 상대차 반폭 이내): 이미 내 차와 겹침/충돌 임박
             v_lat_threshold = 0.2  
-        elif cur_lat <= (lane_edge):  
+        elif cur_lat <= (lane_edge + 0.1):  
             # ② 중간 구역 (0.95m 초과 ~ 차선 반폭): 슬금슬금 좁혀오는 차량 방어
             v_lat_threshold = -0.1  
         elif cur_lat <= (lane_edge + 0.5):  
