e91c42935856bed41a4524cc025df20e714e5e54
Babysurfer-cyber <dlwlsgh0201@gmail.com>
2026-08-26 08:28:10 +0900

    방지턱 감속 보완

--- selfdrive/carrot/carrot_serv.py
+++ selfdrive/carrot/carrot_serv.py
@@ -1035,7 +1035,7 @@ class CarrotServ:
         fake_bump_dist = max(0.0, bump_dist - 10.0)
 
         # 2. 방지턱은 카메라보다 더 부드럽고 일찍 감속하도록 감속률을 낮춥니다 (기본 설정값의 70% 수준)
-        bump_decel_rate = self.autoNaviSpeedDecelRate * 0.6
+        bump_decel_rate = self.autoNaviSpeedDecelRate * 0.8
 
         vehicle_bump_speed = self.calculate_current_speed(fake_bump_dist,
                                                           self.autoNaviSpeedBumpSpeed,
