ee7921049113ac202a83fb9248ae4dd49d646dbd
Babysurfer-cyber <dlwlsgh0201@gmail.com>
2026-09-15 08:12:21 +0900

    모델 감속 수정

--- selfdrive/carrot/carrot_serv.py
+++ selfdrive/carrot/carrot_serv.py
@@ -1123,7 +1123,7 @@ class CarrotServ:
     else:
       # 기존 모드: 오리지널 로직 유지
       model_turn_speed = max(sm['modelV2'].meta.modelTurnSpeed, self.autoCurveSpeedLowerLimit) * 1.1
-      if model_turn_speed < 200:
+      if model_turn_speed < 200 and abs(vturn_speed) < 150:
         speed_n_sources.append((model_turn_speed, "MODEL"))
 
     desired_speed, source = min(speed_n_sources, key=lambda x: x[0])
